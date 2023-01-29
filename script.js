ad893a721f30e2042a022d7345f159c48750a5c3
LukaTm <119138345+LukaTm@users.noreply.github.com>
2023-01-30 01:46:28 +0200

    Works with many numbers

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const multiply = (mult1,mult2) => screenDisplay.textContent = mult1 * mult2;
 const divide = (div1,div2) => screenDisplay.textContent = div1/div2;
 
 //
-let storage = '' // 20
+let storage = '' // 50
 let storage2 = ''
 let userOperation
 let display = ''
@@ -24,39 +24,47 @@ for (x= 0;x< numList.length;x++){
     num.addEventListener("click", () => displayNum(num.textContent));
 }
 
-
+// userOperation
 // Buttons -+/*
 const operatorList = ['divide','multiply','subtract','add']
 for (x= 0;x< 4;x++){
     const operatorSelect = document.querySelector(`.${operatorList[x]}`)
     operatorSelect.addEventListener("click", () => {
-        if (storage !== ''){
+        if (storage !== '' && display !== ''){
             calc()
+            userOperation = operatorSelect.className
+            storage = screenDisplay.textContent
+            display = ''
+            console.log('this display' + display)
+            console.log('this storage' + storage)
+            console.log(userOperation)
         }
         else {
             storage = display;
-            userOperation = operatorSelect.className
             display = ''
+            userOperation = operatorSelect.className
+            console.log('this display2' + display)
+            console.log('this storage2' + storage)
         }
     });
 }
 
 function calc(){
     if (userOperation == 'add') 
-    add(Number(storage),Number(display));
+        add(Number(storage),Number(display));
     if (userOperation == 'subtract') 
         subtract(Number(storage),Number(display));
     if (userOperation == 'divide') 
         divide(Number(storage),Number(display));
     if (userOperation == 'multiply') 
         multiply(Number(storage),Number(display));
+    
 }
 
 const equalSign = document.querySelector('.equal')
-function calculate(){
-    equalSign.addEventListener("click", () => {
-        calc()
-    })
-}
-calculate()
+equalSign.addEventListener("click", () => {
+    calc()
+})
+
+
 
