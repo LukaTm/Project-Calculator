abd7f602f1148b2e85dcf4cd07041f879f50724f
LukaTm <119138345+LukaTm@users.noreply.github.com>
2023-01-31 04:09:30 +0200

    Fix bug when always uses decimal points

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 const screenDisplay = document.querySelector('.display')
 const clearButton = document.querySelector('.clear')
 const deleteButton = document.querySelector('.delete')
+const dotButton = document.querySelector('.dot')
 
 
 // Math function
-const add = (add1,add2) => screenDisplay.textContent = (add1+add2).toFixed(2);
-const subtract = (sub1,sub2) => screenDisplay.textContent = (sub1 - sub2).toFixed(2);
-const multiply = (mult1,mult2) => screenDisplay.textContent = (mult1 * mult2).toFixed(2);
-const divide = (div1,div2) => screenDisplay.textContent = (div1/div2).toFixed(2);
+const add = (add1,add2) => screenDisplay.textContent = (add1+add2);
+const subtract = (sub1,sub2) => screenDisplay.textContent = (sub1 - sub2);
+const multiply = (mult1,mult2) => screenDisplay.textContent = (mult1 * mult2);
+const divide = (div1,div2) => screenDisplay.textContent = (div1/div2);
+
+const add2 = (add1,add2) => screenDisplay.textContent = (add1+add2).toFixed(2);
+const subtract2 = (sub1,sub2) => screenDisplay.textContent = (sub1 - sub2).toFixed(2);
+const multiply2 = (mult1,mult2) => screenDisplay.textContent = (mult1 * mult2).toFixed(2);
+const divide2 = (div1,div2) => screenDisplay.textContent = (div1/div2).toFixed(2);
 
 
-let storage = '' 
+let storage = ''
 let userOperation
 let display = ''
 function displayNum(num){
     display += num;
     screenDisplay.textContent = display;
 }
+// Dot BUtton
+dotButton.addEventListener("click", () => {
+    if (display[display.length - 1] == '.')
+        return;
+    else 
+        displayNum('.');
+})
 
 // Buttons 0-9
 const numList = ['zero','one','two','three','four','five','six','seven','eight','nine']
@@ -31,12 +44,12 @@ clearButton.addEventListener("click", () => {
     screenDisplay.textContent = ''
 })
 
-// Buttons -+/*
+// Delete Button
 deleteButton.addEventListener("click", () => {
     let newDisplay = screenDisplay.textContent = screenDisplay.textContent.slice(0,-1)
     display = newDisplay
 })
-
+// Buttons -+/*
 const operatorList = ['divide','multiply','subtract','add']
 for (x= 0;x< 4;x++){
     const operatorSelect = document.querySelector(`.${operatorList[x]}`)
@@ -54,25 +67,37 @@ for (x= 0;x< 4;x++){
             storage = screenDisplay.textContent;
             display = ''
             userOperation = operatorSelect.className
-            console.log('this display2' + display)
-            console.log('this storage2' + storage)
         }
         checkEqual = 0
     });
 }
 // Calculates two numbers
+
 function calc(){
     if (display == 0)
         return screenDisplay.textContent = 0
-    if (userOperation == 'add') 
-        add(Number(storage),Number(display));
-    if (userOperation == 'subtract') 
-        subtract(Number(storage),Number(display));
-    if (userOperation == 'divide') 
-        divide(Number(storage),Number(display));
-    if (userOperation == 'multiply') 
-        multiply(Number(storage),Number(display));
-    
+    if (screenDisplay.textContent.indexOf(".") !== -1){
+        console.log('Passed')
+        if (userOperation == 'add') 
+            add2(Number(storage),Number(display));
+        if (userOperation == 'subtract') 
+            subtract2(Number(storage),Number(display));
+        if (userOperation == 'divide') 
+            divide2(Number(storage),Number(display));
+        if (userOperation == 'multiply') 
+            multiply2(Number(storage),Number(display));
+    }
+    else{
+        if (userOperation == 'add') 
+            add(Number(storage),Number(display));
+        if (userOperation == 'subtract') 
+            subtract(Number(storage),Number(display));
+        if (userOperation == 'divide') 
+            divide(Number(storage),Number(display));
+        if (userOperation == 'multiply') 
+            multiply(Number(storage),Number(display));
+    }
+
 }
 
 // Equal sign
@@ -86,8 +111,6 @@ equalSign.addEventListener("click", () => {
         calc()
         storage = screenDisplay.textContent
         display = ''
-        console.log('this displayyy' + display)
-        console.log('this storageee' + storage)
         checkEqual = 1
     }
 
